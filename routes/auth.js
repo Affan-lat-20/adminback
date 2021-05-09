60a7787f1a5c3cdd37e44a31390caf5fa9d178ef
Affan-lat-20 <72376364+Affan-lat-20@users.noreply.github.com>
2021-05-09 21:57:46 +0500

    corrected delete api

--- routes/auth.js
+++ routes/auth.js
@@ -82,7 +82,7 @@ router.get('/rolebase',RolebaseController.findrole);
 
 router.get('/:id/rolemanagment/:module/:met',getauthUser,RolebaseController.findrole);
 router.put('rolebase/:id',RolebaseController.roleedit);
-router.delete('rolebase.:id',RolebaseController.Rolebasedelete);
+router.delete('rolebase/:id',RolebaseController.Rolebasedelete);
 
 
 //===============================================ROlenames//
